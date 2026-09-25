4aa73479b16bfbadc20beb39df3b731ad82bfd14
rafaelancelmocastro-byte <rafaelancelmo.castro@gmail.com>
2026-09-25 15:47:20 -0300

    Adjust padding in AppLayout for safe area insets
    Update padding to accommodate safe area insets.

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -56,7 +56,7 @@ export const AppLayout: React.FC<AppLayoutProps> = ({
       />
 
       {/* Conteúdo Principal com Header e Scroll independente */}
-      <div className="flex-1 flex flex-col min-w-0 pb-20 md:pb-8">
+      <div className="flex-1 flex flex-col min-w-0 pb-[calc(5rem+env(safe-area-inset-bottom,0px))] md:pb-8">
         <Header
           searchQuery={searchQuery}
           onSearchChange={onSearchChange}
